663123c18664a5fa4e4fe180c362e0bbf4f57ceb
yan-zhuang2 <yan.zhuang@mail.polimi.it>
2023-05-07 19:52:23 +0200

    up

---
+++ html/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import Notifications from '@kyvg/vue3-notification'
+
+const app = createApp(App)
+app.use(Notifications)
+// test
+app.mount('#app')
